876faa69f28e4c3587828ae74944b0a28a1ce22f
Maksym Yarema <macsym2@gmail.com>
2023-08-22 15:07:32 +0300

    feat(DemoApp.): testing method

--- src/main/java/com/xsakon/demo/DemoApplication.java
+++ src/main/java/com/xsakon/demo/DemoApplication.java
@@ -1,7 +1,17 @@
 package com.xsakon.demo;
 
+import com.xsakon.demo.student.StudentRepository;
+import com.xsakon.demo.student.models.Address;
+import com.xsakon.demo.student.models.Gender;
+import com.xsakon.demo.student.models.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +20,21 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository studentRepository) {
+		return args -> {
+			Student student = new Student(
+					"Walter",
+					"White",
+					Gender.MALE,
+					"heisenberg@gmail.com",
+					new Address("USA", "Albuquerque", "87101"),
+					LocalDateTime.now(),
+					List.of("Chemistry", "Physics"),
+					BigDecimal.TEN
+			);
+
+			studentRepository.insert(student);
+		};
+	}
 }
