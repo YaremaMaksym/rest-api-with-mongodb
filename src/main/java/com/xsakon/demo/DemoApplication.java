52f6308315fb3702022bf57724c0f41b68d449a7
Maksym Yarema <macsym2@gmail.com>
2023-08-23 14:46:50 +0300

    ref(DemoApp): comment test methods

--- src/main/java/com/xsakon/demo/DemoApplication.java
+++ src/main/java/com/xsakon/demo/DemoApplication.java
@@ -23,48 +23,48 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner(StudentRepository studentRepository, MongoTemplate mongoTemplate) {
-		return args -> {
-			String email = "heisenberg@gmail.com";
-			Student student = new Student(
-					"Walter",
-					"White",
-					Gender.MALE,
-					email,
-					new Address("USA", "Albuquerque", "87101"),
-					LocalDateTime.now(),
-					List.of("Chemistry", "Physics"),
-					BigDecimal.TEN
-			);
-
-//			checkIfStudentWithEmailExistsUsingMongoTemplateAndQuery(studentRepository, mongoTemplate, email, student);
-
-			studentRepository.findStudentByEmail(email)
-					.ifPresentOrElse( s -> {
-						System.out.println(s + " already exists");
-						}, () -> {
-						System.out.println("Inserting student " + student);
-						studentRepository.insert(student);
-					});
-		};
-	}
-
-	private static void checkIfStudentWithEmailExistsUsingMongoTemplateAndQuery(StudentRepository studentRepository, MongoTemplate mongoTemplate, String email, Student student) {
-		Query query = new Query();
-		query.addCriteria(Criteria.where("email").is(email));
-
-		List<Student> students = mongoTemplate.find(query, Student.class);
-
-		if (students.size() > 1) {
-			throw new IllegalStateException("found more than one student with that email " + email);
-		}
-
-		if (students.isEmpty()) {
-			System.out.println("Inserting student " + student);
-			studentRepository.insert(student);
-		} else {
-			System.out.println(student + " already exists");
-		}
-	}
+//	@Bean
+//	CommandLineRunner runner(StudentRepository studentRepository, MongoTemplate mongoTemplate) {
+//		return args -> {
+//			String email = "heisenberg@gmail.com";
+//			Student student = new Student(
+//					"Walter",
+//					"White",
+//					Gender.MALE,
+//					email,
+//					new Address("USA", "Albuquerque", "87101"),
+//					LocalDateTime.now(),
+//					List.of("Chemistry", "Physics"),
+//					BigDecimal.TEN
+//			);
+//
+////			checkIfStudentWithEmailExistsUsingMongoTemplateAndQuery(studentRepository, mongoTemplate, email, student);
+//
+//			studentRepository.findStudentByEmail(email)
+//					.ifPresentOrElse( s -> {
+//						System.out.println(s + " already exists");
+//						}, () -> {
+//						System.out.println("Inserting student " + student);
+//						studentRepository.insert(student);
+//					});
+//		};
+//	}
+//
+//	private static void checkIfStudentWithEmailExistsUsingMongoTemplateAndQuery(StudentRepository studentRepository, MongoTemplate mongoTemplate, String email, Student student) {
+//		Query query = new Query();
+//		query.addCriteria(Criteria.where("email").is(email));
+//
+//		List<Student> students = mongoTemplate.find(query, Student.class);
+//
+//		if (students.size() > 1) {
+//			throw new IllegalStateException("found more than one student with that email " + email);
+//		}
+//
+//		if (students.isEmpty()) {
+//			System.out.println("Inserting student " + student);
+//			studentRepository.insert(student);
+//		} else {
+//			System.out.println(student + " already exists");
+//		}
+//	}
 }
